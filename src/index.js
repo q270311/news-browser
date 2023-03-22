567a8f981cb00a0f2d753c872fafdbd22ef5a362
Michał Bałdyga <q270311@gmail.com>
2023-03-22 12:49:40 +0100

    Add ThemeProvider and GlobalStyle to index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { GlobalStyle } from "./core/GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./core/theme";
 import './index.css';
-import App from './App';
+import App from './core/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+        <GlobalStyle />
+          <App />
+      </ThemeProvider>
   </React.StrictMode>
 );
 
