7aedb9f5c76ce17caad4cd52ebbe2aa3d61f61c3
Michał Bałdyga <q270311@gmail.com>
2023-03-22 12:48:59 +0100

    add theme.js file

---
+++ src/core/theme.js
@@ -0,0 +1,14 @@
+export const theme = {
+  color: {
+    black: "#18181B",
+    white: "#FFFFFF",
+    grey: "#7E839A",
+    mercury: "#F5F5FA",
+  },
+  
+  breakpoint: {
+    tabletDevice: 992,
+    mobileDevice: 767,
+    smallDevice: 550,
+  },
+};
