79ef41088f461f0ab8a17a293fe7039cae00787a
Michał Bałdyga <q270311@gmail.com>
2023-03-23 11:16:46 +0100

    Menu styled

--- src/features/Menu/index.js
+++ src/features/Menu/index.js
@@ -1,17 +1,26 @@
-import { StyledMenu } from "./styled";
+import { StyledMenu, Title, List, Item, StyledActiveLink } from "./styled";
 
-const Menu = () => {
-  return (
-      <StyledMenu>
-        <ul>
-          <li>US</li>
-          <li>PL</li>
-          <li>EN</li>
-          <li>DE</li>
-          <li>FR</li>
-        </ul>
-      </StyledMenu>
-  );
-};
+const Menu = () => (
+  <StyledMenu>
+    <Title>Country</Title>
+    <List>
+      <Item>
+        <StyledActiveLink to={`/country/US`} > US </StyledActiveLink>
+      </Item>
+      <Item>
+        <StyledActiveLink to={`/country/PL`} > PL </StyledActiveLink>
+      </Item>
+      <Item>
+        <StyledActiveLink to={`/country/EN`} > EN </StyledActiveLink>
+      </Item>
+      <Item>
+        <StyledActiveLink to={`/country/DE`} > DE </StyledActiveLink>
+      </Item>
+      <Item>
+        <StyledActiveLink to={`/country/FR`} > FR </StyledActiveLink>
+      </Item>
+    </List>
+  </StyledMenu>
+);
 
 export default Menu;
--- src/features/Menu/styled.js
+++ src/features/Menu/styled.js
@@ -1,7 +1,47 @@
 import styled from "styled-components";
+import { NavLink } from 'react-router-dom';
 
 export const StyledMenu = styled.menu`
-
+  margin: 0px;
+  display: flex;
+  flex-direction:column;
   background: ${({ theme }) => theme.color.mercury};
   color: ${({ theme }) => theme.color.black};  
+`;
+
+export const Title = styled.p`
+  font-size: 20px;
+  text-align: center;
+  background: ${({ theme }) => theme.color.mercury};
+  color: ${({ theme }) => theme.color.black};  
+`;
+
+export const List = styled.ul`
+    width: 100%;
+    list-style: none;
+    padding:0px;
+`;
+
+export const Item = styled.li`
+    display: flex;
+    justify-content: center;
+    font-size: 15px;
+    border-bottom: 1px solid ${({ theme }) => theme.color.mercury};
+    padding: 10px 0px;
+`;
+const activeClassName = "active";
+export const StyledActiveLink = styled(NavLink).attrs(() => ({
+  activeClassName,
+}))`
+    color: ${({ theme }) => theme.color.black};
+    text-decoration: none;
+
+    &.${activeClassName}{
+        font-weight: bold;
+    };
+    
+
+    &:hover{
+        font-weight: bold;
+    };
 `;
