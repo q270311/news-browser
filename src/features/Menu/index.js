aa47ff66fb0e809c8a5e7421f925e2c35993180b
Michał Bałdyga <q270311@gmail.com>
2023-03-23 08:33:19 +0100

    Menu component created

---
+++ src/features/Menu/index.js
@@ -0,0 +1,17 @@
+import { StyledMenu } from "./styled";
+
+const Menu = () => {
+  return (
+      <StyledMenu>
+        <ul>
+          <li>US</li>
+          <li>PL</li>
+          <li>EN</li>
+          <li>DE</li>
+          <li>FR</li>
+        </ul>
+      </StyledMenu>
+  );
+};
+
+export default Menu;
---
+++ src/features/Menu/styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const StyledMenu = styled.menu`
+
+  background: ${({ theme }) => theme.color.mercury};
+  color: ${({ theme }) => theme.color.black};  
+`;
