75386f1c58283aede5e3b47386440365c4fe5d8f
Michał Bałdyga <q270311@gmail.com>
2023-03-23 08:32:01 +0100

    MainWrapper component created

---
+++ src/common/MainWrapper/index.js
@@ -0,0 +1,10 @@
+import { PageWrapper } from "./styled";
+
+export const MainWrapper = ({ header, menu, content, footer }) => (
+  <PageWrapper>
+    {header}
+    {menu}
+    <div> {content} </div>
+    {footer}
+  </PageWrapper>
+);
---
+++ src/common/MainWrapper/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const PageWrapper = styled.div`
+  max-width: 1400px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 30% 1fr;
+  grid-row: auto;
+  grid-gap: 20px;
+`;
