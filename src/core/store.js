3835c759bc99eb17692a6e4b5c6475bb7fe78cc9
Michał Bałdyga <q270311@gmail.com>
2023-03-24 13:30:13 +0100

    add Saga to App

--- src/core/store.js
+++ src/core/store.js
@@ -1,10 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import newsListReducer from '../features/newsListSlice';
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "./rootSaga";
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
     reducer: {
         news: newsListReducer,
     },
+    middleware: [sagaMiddleware],
 });
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
---
+++ src/features/newsListSaga.js
@@ -0,0 +1,17 @@
+import { call, put, takeLatest, select } from "redux-saga/effects";
+import { selectCountry, setCountry, fetchDataSuccess, fetchDataError } from "./newsListSlice";
+import { searchNews } from "../core/getDataFromNewsAPI";
+
+function* fetchNewsHandler() {
+    try {
+        const countryCode = yield select(selectCountry);
+        const data = yield call(searchNews, { countryCode: countryCode });
+        yield put(fetchDataSuccess({ data }));
+    } catch (error) {
+        yield put(fetchDataError());
+    }
+}
+
+export function* watchFetchNews() {
+    yield takeLatest(setCountry.type, fetchNewsHandler);
+}
