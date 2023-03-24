f1f26fed7f64972b9124f6fd7eea2e2ecb41b2cd
Michał Bałdyga <q270311@gmail.com>
2023-03-24 13:35:39 +0100

    join CountryNews component with store

--- src/features/CountryNews/index.js
+++ src/features/CountryNews/index.js
@@ -1,19 +1,38 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
+import { setCountry, selectCountry, selectArticles } from "../newsListSlice";
 import { MainWrapper } from '../../common/MainWrapper';
 import Header from "../Header";
 import Footer from "../Footer";
 import Menu from "../Menu";
+import { nanoid } from 'nanoid';
 
 const CountryNews = () => {
-    const  {countryCode}  = useParams();
-    
+    const dispatch = useDispatch();
+    const { countryCode } = useParams();
+    const articles = useSelector(selectArticles);
+
+    useEffect(() => {
+        dispatch(setCountry({ countryCode: countryCode }));
+    }, [countryCode, dispatch]); 
+
     return (
         <MainWrapper
-            header={<Header/>}
+            header={<Header />}
             menu={<Menu />}
-            content={
-                <p>Breaking news from {countryCode}</p>
-            }
+            content={<>
+                <h1>News from {useSelector(selectCountry)}</h1>
+                <ul>
+                    {articles.map(article => (
+                        <li key={nanoid()}>
+                            Tytuł: {article.title},
+                            Źródło: {article.author},
+                            Data publikacji: {article.publishedAt}
+                        </li>
+                    ))}
+                </ul>
+            </>}
             footer={<Footer />}
         />
     );
