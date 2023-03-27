4202816df34af8f4621d9e9dcf73ee077f6b3f51
Michał Bałdyga <q270311@gmail.com>
2023-03-27 22:39:16 +0200

    getApi functin created

--- src/features/News/getDataFromNewsAPI.js
+++ src/features/News/getDataFromNewsAPI.js
@@ -3,31 +3,36 @@ import axios from "axios";
 const BASE_URL = "https://newsapi.org/v2";
 const API_KEY = "dd008e88cbfc475293656969c938e744";
 
-// poniższy kod powoduje błąd 401, na ten moment musi chwilę poczekac na rozwiązanie
+const getApi = async (path, params) => {
+    const response = await axios.get(`${BASE_URL}${path}`, {
+        params: {
+            apiKey: API_KEY,
+            ...params,
+        },
+    });
 
-/*   const getApi = async (path, params) => {
-     const response = await axios.get(`${BASE_URL}${path}`, {
-         params: {                
-             ...params,
-             api_key: API_KEY,
-         },
-     });
-
-     if (!response.ok) {
-         throw new Error(response.statusText);
-     }
-
-     return response.data;
- }
-
- export const searchNews = ({countryCode})=> getApi('/top-headlines/',{ "country": countryCode } );  */
-
-export const searchNews = async ({ countryCode }) => {
-    const response = await axios.get(`${BASE_URL}/top-headlines?country=${countryCode}&apiKey=${API_KEY}`);
-
+    console.log(response);
     if (!response.ok) {
         new Error(response.statusText);
     }
 
     return await response.data;
-};  
+}
+
+export const searchNews = ({ countryCode }) => getApi('/top-headlines', { "country": countryCode });
+
+/* export const searchNews = async ({ countryCode }) => {
+    const params = new URLSearchParams({
+        apiKey: API_KEY,
+        country: countryCode,
+    });
+
+    const response = await axios.get(`${BASE_URL}/top-headlines?${params}`);
+
+    console.log(response);
+    if (!response.ok) {
+        new Error(response.statusText);
+    }
+
+    return await response.data;
+}; */
