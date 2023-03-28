1d121e19caaacfa293b25915d469f20434431d1b
Michał Bałdyga <q270311@gmail.com>
2023-03-28 21:57:46 +0200

    PopupProvider added

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from './core/store';
+import store from "./core/store";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./core/GlobalStyle";
 import { theme } from "./core/theme";
-import App from './core/App';
-import reportWebVitals from './reportWebVitals';
+import { PopupProvider } from "react-custom-popup";
+import App from "./core/App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <PopupProvider>
+          <GlobalStyle />
+          <HashRouter>
+            <App />
+          </HashRouter>
+        </PopupProvider>
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
