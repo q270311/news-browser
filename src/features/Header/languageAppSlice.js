972eaa38fba0f546fc65f81a692633d718351ad3
Michał Bałdyga <q270311@gmail.com>
2023-03-30 09:00:06 +0200

    languageAppSlice added

---
+++ src/features/Header/languageAppSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { getLanguageFromLocalStorage } from "./languageLocalStorage";
+
+const languageAppSlice = createSlice({
+    name: "languageApp",
+    initialState: {
+        language: getLanguageFromLocalStorage(),
+    },
+    reducers: {
+        toggleLanguageApp: (state, { payload }) => {
+            state.language = payload;
+        },
+    },
+});
+
+export const {
+    toggleLanguageApp,
+} = languageAppSlice.actions;
+
+export const selectLanguageSlice = (state) => state.languageApp;
+export const selectLanguageApp = (state) => selectLanguageSlice(state).language;
+
+export default languageAppSlice.reducer;
