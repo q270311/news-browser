4ac575aa5875715414f7ef5e401f9f2514ff8111
Michał Bałdyga <q270311@gmail.com>
2023-03-30 15:35:28 +0200

    add transcription to welcome site

--- src/features/News/WelcomeSite/index.js
+++ src/features/News/WelcomeSite/index.js
@@ -1,22 +1,27 @@
-import { MainWrapper } from '../../../common/MainWrapper';
+import { MainWrapper } from "../../../common/MainWrapper";
 import Header from "../../Header";
 import Footer from "../../Footer";
 import Menu from "../../Menu";
-import {Welcome} from './styled';
+import { Welcome } from "./styled";
+import { useTranslation } from "../../../core/useTranslation";
 
-const WelcomeSite = () => (
+const WelcomeSite = () => {
+  const translation = useTranslation();
+
+  return (
     <MainWrapper
-        header={<Header />}
-        menu={<Menu />}
-        content={
-            <Welcome>
-                <h1>Wyszukuj wiadomości z całego świta</h1>
-                <h2>Czytaj artykuły i najświeższe nagłówki</h2>
-                <p>Aby rozpocząć, wybierz kraj z lewego menu</p>
-            </Welcome>
-        }
-        footer={<Footer />}
+      header={<Header />}
+      menu={<Menu />}
+      content={
+        <Welcome>
+          <h1>{translation.welcomeMessage.line1}</h1>
+          <h2>{translation.welcomeMessage.line2}</h2>
+          <p>{translation.welcomeMessage.line3}</p>
+        </Welcome>
+      }
+      footer={<Footer />}
     />
-);
+  );
+};
 
 export default WelcomeSite;
