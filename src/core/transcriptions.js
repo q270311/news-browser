e92a70380eee156e62180db3b1514c9c0f896309
Michał Bałdyga <q270311@gmail.com>
2023-03-30 15:33:39 +0200

    transcriptions table added

---
+++ src/core/transcriptions.js
@@ -0,0 +1,38 @@
+export const transcriptions = [
+  {
+    language: "PL",
+    welcomeMessage: {
+      line1: "Wyszukuj wiadomości z całego świta",
+      line2: "Czytaj artykuły i najświeższe nagłówki",
+      line3: "Aby rozpocząć, wybierz pańtwo z lewego menu",
+    },
+    menu:{
+      title:"Państwo",
+      countries:[
+        {name:"Stany Zjednoczone", symbol:"USA",},
+        {name:"Polska", symbol:"PL",},
+        {name:"Wielka Brytania", symbol:"EN",},
+        {name:"Niemcy", symbol:"DE",},
+        {name:"Francja", symbol:"FR"},
+      ],
+    }
+  },
+  {
+    language: "EN",
+    welcomeMessage: {
+      line1: "Search worldwide news",
+      line2: "Locate articles and breaking news headlines from news",
+      line3: "To start, select country from left bar.",
+    },
+    menu:{
+      title:"Countries",
+      countries:[
+        {name:"United States", symbol:"USA",},
+        {name:"Poland", symbol:"PL",},
+        {name:"Great Britain", symbol:"EN",},
+        {name:"Germany", symbol:"DE",},
+        {name:"France",  symbol:"FR",},
+      ],
+    }
+  },
+];
