e3ce4702fa684d1ea1e0f8b8fd983cf553df9dc9
Michał Bałdyga <q270311@gmail.com>
2023-03-31 14:08:37 +0200

    InfoButton component add

--- src/features/Header/index.js
+++ src/features/Header/index.js
@@ -1,6 +1,7 @@
 import { StyledHeader, NavigationLink} from "./styled";
 import ListTileToggleButton from './ViewToggleButton';
-import LanguageToggleButton from './LanguageToggleButton'
+import LanguageToggleButton from './LanguageToggleButton';
+import InfoButton from './InfoButton';
 
 const Header = () => {
   return (
@@ -8,6 +9,7 @@ const Header = () => {
         <NavigationLink to="/" >
           News Browser
         </NavigationLink>
+        <InfoButton />
         <ListTileToggleButton />
         <LanguageToggleButton />
       </StyledHeader>
