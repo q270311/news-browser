350fbc9931dec347741e6ac652aa471515d97afc
Michał Bałdyga <q270311@gmail.com>
2023-03-31 08:22:14 +0200

    add footer text to table translation

--- src/core/transcriptions.js
+++ src/core/transcriptions.js
@@ -6,16 +6,20 @@ export const transcriptions = [
       line2: "Czytaj artykuły i najświeższe nagłówki",
       line3: "Aby rozpocząć, wybierz pańtwo z lewego menu",
     },
-    menu:{
-      title:"Państwo",
-      countries:[
-        {name:"Stany Zjednoczone", symbol:"USA",},
-        {name:"Polska", symbol:"PL",},
-        {name:"Wielka Brytania", symbol:"EN",},
-        {name:"Niemcy", symbol:"DE",},
-        {name:"Francja", symbol:"FR"},
+    menu: {
+      title: "Państwo",
+      countries: [
+        { name: "Stany Zjednoczone", symbol: "USA" },
+        { name: "Polska", symbol: "PL" },
+        { name: "Wielka Brytania", symbol: "EN" },
+        { name: "Niemcy", symbol: "DE" },
+        { name: "Francja", symbol: "FR" },
       ],
-    }
+    },
+    footer: {
+      numberArticles: "Liczba artykułów na stronie",
+      currentTime: "Aktualny czas",
+    },
   },
   {
     language: "EN",
@@ -24,15 +28,19 @@ export const transcriptions = [
       line2: "Locate articles and breaking news headlines from news",
       line3: "To start, select country from left bar.",
     },
-    menu:{
-      title:"Countries",
-      countries:[
-        {name:"United States", symbol:"USA",},
-        {name:"Poland", symbol:"PL",},
-        {name:"Great Britain", symbol:"EN",},
-        {name:"Germany", symbol:"DE",},
-        {name:"France",  symbol:"FR",},
+    menu: {
+      title: "Countries",
+      countries: [
+        { name: "United States", symbol: "USA" },
+        { name: "Poland", symbol: "PL" },
+        { name: "Great Britain", symbol: "EN" },
+        { name: "Germany", symbol: "DE" },
+        { name: "France", symbol: "FR" },
       ],
-    }
+    },
+    footer: {
+      numberArticles: "Number of articles per page",
+      currentTime: "Current time",
+    },
   },
 ];
